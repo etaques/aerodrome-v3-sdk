855e79de9677a1d367921db4662c9fd85f8e08c1
Everton H. Taques <97463920+etaques@users.noreply.github.com>
2024-10-31 13:07:12 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,21 @@
-export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+export const FACTORY_ADDRESS = '0x5e7BB104d84c7CB9B682AaC2F3d509f5F406809A'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
-export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
+export const POOL_INIT_CODE_HASH = '0xffb9af9ea6d9e39da47392ecc7055277b9915b8bfc9f83f105821b7791a6ae30'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
  */
 export enum FeeAmount {
   LOWEST = 100,
+  LOWMEDIUM = 400,
   LOW = 500,
+  MEDIUMLOW = 2500,
   MEDIUM = 3000,
-  HIGH = 10000
+  HIGHLOW = 6000,
+  HIGH = 10000,
+  HIGHEST = 20000
 }
 
 /**
@@ -19,7 +23,11 @@ export enum FeeAmount {
  */
 export const TICK_SPACINGS: { [amount in FeeAmount]: number } = {
   [FeeAmount.LOWEST]: 1,
+  [FeeAmount.LOWMEDIUM]: 8,
   [FeeAmount.LOW]: 10,
+  [FeeAmount.MEDIUMLOW]: 50,
   [FeeAmount.MEDIUM]: 60,
-  [FeeAmount.HIGH]: 200
+  [FeeAmount.HIGHLOW]: 120,
+  [FeeAmount.HIGH]: 200,
+  [FeeAmount.HIGHEST]: 400
 }
