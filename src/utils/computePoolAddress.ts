bb7aa61a994d6dd97c940b1e4a4315e206987bf0
Everton H. Taques <97463920+etaques@users.noreply.github.com>
2024-10-31 13:27:22 -0300

    Update computePoolAddress.ts

--- src/utils/computePoolAddress.ts
+++ src/utils/computePoolAddress.ts
@@ -13,6 +13,19 @@ import { FeeAmount, POOL_INIT_CODE_HASH } from '../constants'
  * @param initCodeHashManualOverride Override the init code hash used to compute the pool address if necessary
  * @returns The pool address
  */
+export function getTickSpacing(fee: number): number {
+  if (fee === 100) return 1;
+  if (fee === 400) return 8;
+  if (fee === 500) return 10;
+  if (fee === 2500) return 50;
+  if (fee === 3000) return 60;
+  if (fee === 6000) return 120;
+  if (fee === 10000) return 200;
+  if (fee === 20000) return 400;
+
+  throw new Error("Unsupported pool fee.");
+}
+
 export function computePoolAddress({
   factoryAddress,
   tokenA,
@@ -27,11 +40,12 @@ export function computePoolAddress({
   initCodeHashManualOverride?: string
 }): string {
   const [token0, token1] = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
+  const tickSpacing = getTickSpacing(fee);
   return getCreate2Address(
     factoryAddress,
     keccak256(
       ['bytes'],
-      [defaultAbiCoder.encode(['address', 'address', 'uint24'], [token0.address, token1.address, fee])]
+      [defaultAbiCoder.encode(['address', 'address', 'int24'], [token0.address, token1.address, tickSpacing])]
     ),
     initCodeHashManualOverride ?? POOL_INIT_CODE_HASH
   )
